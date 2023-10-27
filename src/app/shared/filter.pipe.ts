be37b594d6d30a64ea97cf539ef87e5833064e67
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-10-27 12:47:21 -0600

    documentacion pipe

--- src/app/shared/filter.pipe.ts
+++ src/app/shared/filter.pipe.ts
@@ -4,8 +4,17 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filter',
 })
 export class FilterPipe implements PipeTransform {
+  /**
+   * Funcion para filtar datos enviados desde un api arroja un array de tipo any
+   * @param value  es el json que manda el api y lo guardamos en un array
+   * @param filterString es la palabra con la que vas a filtrar toda la informacion
+   * @param propName para filtar por propiedad como titulo o categorias
+   * @returns
+   */
   transform(value: any[], filterString: string, propName: string): any[] {
-    const result: any = [];
+    const result: any = []; //almacena la nueva infromacion filtrada a mostrar
+
+    //si los parametros para filtrar estan vacios retornamos la informacion completa (el json que manda el api)
     if (!value || filterString === '' || propName === '') {
       return value;
     }
